0b4f6db2d52782b01298f8a442d3411be463b8ce
Yulia C <yulka1355@gmail.com>
2025-08-30 11:14:30 +0300

    Create fallback route for git pages to help load the site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MisterToy-Frontend/'
+  base: '/MisterToy-Frontend/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 })
