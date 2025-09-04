d8c5b03c926a500f6b975d252f73cde745b9ab84
Yulia C <yulka1355@gmail.com>
2025-09-04 19:22:11 +0300

    Update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => ({
-  plugins: [react()]
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: '/MisterToy-Frontend/', 
+  // build: {
+  //   rollupOptions: {
+  //     input: './MisterToy-Frontend/index.html'
+  //   }
+  // }
 }))
