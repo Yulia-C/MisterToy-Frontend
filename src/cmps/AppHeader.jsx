79751c63779fb8ee174f0b8349781da6c1a35a13
Yulia C <yulka1355@gmail.com>
2025-09-06 15:02:06 +0300

    Add i18 to header nav

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,17 +2,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 
 import { UserMsg } from "./UserMsg.jsx"
-import { LoginSignup } from '../pages/LoginSignup.jsx'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
 import { logout } from '../store/actions/user.actions.js'
-
 import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 
 export function AppHeader() {
     const loggedinUser = useSelector(storeState => storeState.userModule.loggedinUser)
     const [isSignup, setIsSignUp] = useState(false)
     const navigate = useNavigate()
+
+    const { t, i18n } = useTranslation()
+
+    const lngs = {
+        en: { nativeName: 'English' },
+        ru: { nativeName: 'Russian' },
+        he: { nativeName: 'Hebrew' },
+    }
 
     function handleAuthToggle() {
         const newSignupState = !isSignup
@@ -27,29 +34,37 @@ export function AppHeader() {
                 showErrorMsg('OOPs try again')
             })
     }
-    // const formattedPercent = toys ? progressStats.toFixed(2) + '%' : null
 
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
                 <h1>'React' Toy App</h1>
-                <nav className="app-nav">
+                {Object.keys(lngs).map(lng => (
+                    <button
+                        key={lng}
+                        className="btn"
+                        onClick={() => i18n.changeLanguage(lng)}
+                        disabled={i18n.resolvedLanguage === lng}
+                    >
+                        {lngs[lng].nativeName}
+                    </button>
+                ))}
 
-                    <NavLink to="/" >Home</NavLink>
-                    <NavLink to="/about" >About</NavLink>
-                    <NavLink to="/toy" >Toys</NavLink>
-                    <NavLink to="/dashboard" >Dashboard</NavLink>
+                <nav className="app-nav">
+                    <NavLink to="/">{t('home')}</NavLink>
+                    <NavLink to="/toy">{t('toys')}</NavLink>
+                    <NavLink to="/dashboard">{t('dashboard')}</NavLink>
+                    <NavLink to="/about">{t('about')}</NavLink>
 
                     {loggedinUser ? (
-
-                        < section >
-                            <Link to={`/user/${loggedinUser._id}`}>Hello {loggedinUser.fullname} <span className="balance">{loggedinUser.balance}</span></Link>
-                            <button onClick={onLogout}>Logout</button>
+                        < section>
+                            <Link to={`/user/${loggedinUser._id}`}>{t('hello')} {loggedinUser.fullname} <span className="balance">{loggedinUser.balance}</span></Link>
+                            <button onClick={onLogout}>{t('logout')}</button>
                         </ section >
                     ) : (<button onClick={handleAuthToggle}>
                         {isSignup ?
-                            'Already a member? Login' :
-                            'New user? Signup here'}
+                            t('signup') :
+                            t('login')}
                     </button>)
                     }
                 </nav>
---
+++ src/i18n.js
@@ -0,0 +1,51 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next).init({
+        debug: true,
+        lng: "en",
+        resources: {
+            en: {
+                translation: {
+                    home: 'Home',
+                    toys: 'Toys',
+                    dashboard: 'Dashboard',
+                    about: 'About',
+                    hello: 'Hello',
+                    logout: 'Logout',
+                    login: 'Already a member? Login',
+                    signup: 'New user? Signup here',
+                    i18: 'internationalization',
+                }
+            },
+            ru: {
+                translation: {
+                    home: 'Домашняя Страница',
+                    toys: 'Игрушки',
+                    dashboard: 'Панель',
+                    about: 'О нас',
+                    hello: 'Здравствуйте',
+                    logout: 'Выход',
+                    login: 'Уже зарегистрированы? Войти',
+                    signup: 'Новый пользователь? Зарегистрируйтесь здесь.',
+                    i18: 'Интернационализация',
+                }
+            },
+            he: {
+                translation: {
+                    home: 'עמוד הבית',
+                    toys: 'צעצועים',
+                    dashboard: 'סטטיסטיקה',
+                    about: 'קצת עלינו',
+                    hello: 'שלום',
+                    logout: 'צא',
+                    login: 'משתמש רשום? התחבר',
+                    signup: 'משתמש חדש? הירשם פה',
+                    i18: 'internationalization',
+                }
+            }
+        }
+        })
